574a94458587d19fd6cf9414f121c9b562156569
Oksana2607 <k.sushalev26@gmail.com>
2019-09-12 10:18:31 +0300

    hw13

--- public_postgre/js/config.js
+++
@@ -1,9 +0,0 @@
-// export const configPost = {
-//     db: {
-//         host: "plop",
-//         database: "musicbrainz",
-//         username: "admin",
-//         password: "admin",
-//         port: 3000,
-//     },
-// };
